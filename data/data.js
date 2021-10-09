efa7bc0f5c509dc07d58242b2c121cbd6fa770e4
H <leithhamish@gmail.com>
2021-10-09 20:07:25 +0100

    a

--- data/data.js
+++ data/data.js
@@ -4,19 +4,37 @@ data = [
     "id":1,
     "name":"Newspaper",
     "recyclable":true,
-    "description":"Paper is one of the most valuable recyclable materials, but only when it’s clean, so throw dirty paper in your rubbish bin (e.g. greasy/oily paper and paper contaminated with food)."
+    "description":"Paper is one of the most valuable recyclable materials, but only when it’s clean, so throw dirty paper in your rubbish bin (e.g. greasy/oily paper and paper contaminated with food).",
+    "link":"https://londonrecycles.co.uk/a-to-z/paper/"
 },
 {
     "id":2,
     "name":"Magazines",
     "recyclable":true,
-    "description":"Glossy magazines are made out of paper and therefore widely recycled. If you’re done with hoarding them under the bed, pop them in your recycling bin."
+    "description":"Glossy magazines are made out of paper and therefore widely recycled. If you’re done with hoarding them under the bed, pop them in your recycling bin.",
+    "link":"https://londonrecycles.co.uk/a-to-z/paper/"
 },
 {
     "id":3,
     "name":"Film wrap",
     "recyclable":false,
-    "description":"Generally, plastic film and carrier bags can’t be recycled at home, but some can be recycled at supermarket carrier bag collection points. First, check on the packaging to see if they can be recycled or not."
+    "description":"Generally, plastic film and carrier bags can’t be recycled at home, but some can be recycled at supermarket carrier bag collection points. First, check on the packaging to see if they can be recycled or not.",
+    "link":"https://londonrecycles.co.uk/a-to-z/plastic-bags/"
 },
+{
+    "id":4,
+    "name":"Kitchen Roll",
+    "recyclable":false,
+    "description":"The cardboard inner roll of your kitchen towel should go in your recycling bin. The paper towels, used or unused, can’t be recycled and should go in your rubbish bin. If you have a food waste bin, some councils let you put a small amount of kitchen towel in with your food waste, but you should always double check as local collections do vary.",
+    "link":"https://londonrecycles.co.uk/a-to-z/kitchen-towel/"
+},
+{
+    "id":5,
+    "name":"Aerosols",
+    "recyclable":true,
+    "description":"But not all councils collect them for recycling. Check our recycling locator to see what your local collection arrangements are.",
+    "link":"https://londonrecycles.co.uk/a-to-z/aerosols/"
+},
+
 
 ];

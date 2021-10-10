513c9d260cb3346b2ac505872a2f6ca03471d1dd
H <leithhamish@gmail.com>
2021-10-10 14:06:19 +0100

    a

--- data/data.js
+++ data/data.js
@@ -84,5 +84,39 @@ data = [
     "description":"Plastic bottles are pretty much recycled everywhere. Here's a reminder of what to do with them... 1. Make sure they're empty! 2. Empty, rinse and replace the lid. 3.Leave the label on. 4. Squash to save space. 5. Remove pumps from soap bottles and throw in the rubbish. 6. Trigger sprays don't need removing, as these can be recycled",
     "link":"https://londonrecycles.co.uk/a-to-z/plastic-bottles/"
 },
-
+{
+    "id":12,
+    "name":"Crisp Packets",
+    "recyclable":false,
+    "description":"Crisp packets need to go into your rubbish bin as they can’t be recycled. Although the inside of the packet is shiny and looks like foil, it’s actually metallised plastic film, which can’t be recycled from home yet. Terracycle and Walkers run a crisp packet recycling scheme with public drop-off points.",
+    "link":"https://londonrecycles.co.uk/a-to-z/crisp-packets/"
+},
+{
+    "id":14,
+    "name":"Coffee Pods",
+    "recyclable":false,
+    "description":"Single-use pods used in many coffee machines can not normally be recycled. Some brands/types of coffee pods (and other difficult-to-recycle items) however, are collected by a company called TerraCycle, who recycle them to create new products such as plastic baby bibs, notebook covers and park benches. Check them out to find out more about their scheme, what they collect and how you can get involved. If you have a Nespresso machine, you can also take advantage of their recycling scheme, which lets you drop-off or organise a collection of your used pods.",
+    "link":"https://londonrecycles.co.uk/a-to-z/coffee-pods/"
+},
+{
+    "id":15,
+    "name":"Food Pouches",
+    "recyclable":false,
+    "description":"Food and drink pouches, for things like coffee, pet food and baby food can’t be recycled at home. The good news is that some types/brands of pouches are collected by a company called TerraCycle who recycle them to create new products! Their website explains more about their scheme, what they collect and how you can get involved.",
+    "link":"https://londonrecycles.co.uk/a-to-z/food-drink-pouches/"
+},
+{
+    "id":16,
+    "name":"Cling Film",
+    "recyclable":false,
+    "description":"Generally, plastic film and carrier bags can't be recycled at home, but some can be recycled at supermarket carrier bag collection points. First, check on the packaging to see if they can be recycled or not.",
+    "link":"https://londonrecycles.co.uk/a-to-z/plastic-bags/"
+},
+{
+    "id":17,
+    "name":"Cotton Pads",
+    "recyclable":false,
+    "description":"Cotton pads can’t be recycled and should go in your rubbish bin. Don’t try to flush them either, as cotton wool expands in water and can cause blockages. If you’re after a reusable alternative, you can buy reusable make-up remover pads made from fabric. Lots come with wash-bags as well so that you can clean them easily when you next put a clothes load in the washing machine.",
+    "link":"https://londonrecycles.co.uk/a-to-z/cotton-pads/"
+},
 ];
